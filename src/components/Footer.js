c03a74752e62f9a59f99e9d5bf5a8a69af5b3fed
Vishal Kumar <vy10607@gmail.com>
2023-01-09 11:54:18 +0530

    Add anchor tag of github profile in footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer(props) {
   return (
     <div className={`text-center mt-6 ${props.mode==="dark" ? "text-white": "text-black"} font-medium`}>
-      Made with ❤ by Vishal
+      Made with ❤ by <a className="text-green-600" href="https://github.com/vishalkumar08376" rel="noreferrer" target="_blank">Vishal</a>
     </div>
   )
 }
