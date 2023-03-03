a5554293782f6a78bf3faaa5f11ae2b1c154af14
Vishal Kumar <96040765+vishal-kumaar@users.noreply.github.com>
2023-03-03 21:29:01 +0530

    Update github profile link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer(props) {
   return (
     <div className={`text-center mt-6 ${props.mode==="dark" ? "text-white": "text-black"} font-medium`}>
-      Made with ❤ by <a className="text-green-600" href="https://github.com/vishalkumar08376" rel="noreferrer" target="_blank">Vishal</a>
+      Made with ❤ by <a className="text-green-600" href="https://github.com/vishal-kumaar" rel="noreferrer" target="_blank">Vishal</a>
     </div>
   )
 }
